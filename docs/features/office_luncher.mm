d4ddf3a3bd4c58ad8f524a32f5ff3383da997bee
Marcin Krasowski <marcin.krasowski@vimn.com>
2018-06-05 18:38:31 +0200

    initial feature set

---
+++ docs/features/office_luncher.mm
@@ -0,0 +1,89 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="office_luncher" FOLDED="false" ID="ID_1126226545" CREATED="1527880138664" MODIFIED="1527880245464" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="features" POSITION="right" ID="ID_1773929335" CREATED="1527880269299" MODIFIED="1527880272464">
+<edge COLOR="#ff0000"/>
+<node TEXT="new user autoregistration" ID="ID_630021734" CREATED="1527880273493" MODIFIED="1527880281811">
+<node TEXT="new user can create his/her favourite places list" ID="ID_719769563" CREATED="1527880288675" MODIFIED="1527880308199"/>
+<node TEXT="new user can invite other users" ID="ID_496337173" CREATED="1527880311097" MODIFIED="1527880326774"/>
+</node>
+<node TEXT="user autoregistratin with invitation" ID="ID_547974807" CREATED="1527880328454" MODIFIED="1527880340086">
+<node TEXT="invited user can add his/her favourite places" ID="ID_828453507" CREATED="1527880345007" MODIFIED="1527880369685"/>
+</node>
+<node TEXT="lunch time" ID="ID_1294260345" CREATED="1527880376960" MODIFIED="1527882484191">
+<node TEXT="any of the users starts voting by casting a first vote" ID="ID_794738275" CREATED="1527882485842" MODIFIED="1527882509609"/>
+<node TEXT="other users can join by casting their votes in configurable voting time" ID="ID_1318417956" CREATED="1527882516332" MODIFIED="1527882549501"/>
+<node TEXT="vote results are avaiable in near real time" ID="ID_1057071468" CREATED="1527882550584" MODIFIED="1527882568483"/>
+<node TEXT="when voting time is over the menu of the place that won the election phase is displayed" ID="ID_653744541" CREATED="1527882577908" MODIFIED="1527882641591"/>
+<node TEXT="every user can choose from the menu for her/his" ID="ID_679342083" CREATED="1527882648310" MODIFIED="1527882676119"/>
+<node TEXT="after configurable time ordering phase is over and the order can be placed" ID="ID_1509455743" CREATED="1527882677699" MODIFIED="1527882728812">
+<node TEXT="each user who participated in the order is charged for the meal" ID="ID_848427502" CREATED="1527882890763" MODIFIED="1527882925736"/>
+</node>
+<node TEXT="the mchanism to place the order and charge individual accounts to share the costs needs to be decided" ID="ID_107900853" CREATED="1527882854239" MODIFIED="1527882886112"/>
+</node>
+</node>
+</node>
+</map>
